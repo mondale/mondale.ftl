dcebdefddaa7edde6a2a8ed364b26d4581260ef6
The Mondale <the_mondale@proton.me>
2026-09-02 21:12:40 -0500

    sigsegv during encode

--- capsule/by_hand_test.cc
+++ capsule/by_hand_test.cc
@@ -295,6 +295,7 @@ TEST(MsvTest) {
   ASSERT_EQ(framed_storage_size, span->n());
   ASSERT_EQ(reinterpret_cast<uintptr_t>(span->DataAsPtrTo<void>()) % 8, 0);
 
+  // Encode.
   capsule::Encoder e(span->DataAsPtrTo<void>(), capsule_storage_size,
                      TopLevelM::kFieldCount);
   e.Add(TopLevelM::kTypeHash, *m);
