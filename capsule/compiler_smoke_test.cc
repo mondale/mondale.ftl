11cce922ef2817eae21580d7bf99a4b7249ccc42
The Mondale <the_mondale@proton.me>
2026-09-06 21:01:43 -0500

    holy crap the compiler now works

--- capsule/compiler_smoke_test.cc
+++ capsule/compiler_smoke_test.cc
@@ -1,9 +1,10 @@
+#include "capsule/testdata/generator_smoke_test.capsule.h"
 #include "testing/testing.h"
 
 namespace {
 
 TEST(DoesItWork) {
-  //
+  test_ns::FullFeatureM m;  // I guess it works if this compiles.
 }
 
 }  // namespace
