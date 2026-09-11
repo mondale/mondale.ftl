dca2d5390db7d234c81b0b96100b467f208004cc
The Mondale <the_mondale@proton.me>
2026-09-11 15:19:23 -0500

    rudimentry capsule tostring

--- capsule/compiler_positive_test.cc
+++ capsule/compiler_positive_test.cc
@@ -1,10 +1,20 @@
 #include "capsule/compiler_smoke_test.capsule.h"
 #include "testing/testing.h"
 
+using testing::HasSubstr;
+
 namespace {
 
 TEST(DoesItWork) {
   test_ns::FullFeatureM m;  // I guess it works if this compiles.
+}
+
+TEST(ToStringLittlePair) {
+  test_ns::LittlePairM m;
+  m.crc = 4;
+  m.name = "Jolly";
+  EXPECT_THAT(m.ToString(), HasSubstr("crc: 4"));
+  EXPECT_THAT(m.ToString(), HasSubstr("name: \"Jolly\""));
 }
 
 }  // namespace
