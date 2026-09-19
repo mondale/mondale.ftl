1ba5e18e59412cc8b340ecec33b269738638e1fb
The Mondale <the_mondale@proton.me>
2026-09-19 13:15:00 -0500

    context test

--- io/context_test.cc
+++ io/context_test.cc
@@ -1,10 +1,19 @@
 #include "io/context.h"
 #include "testing/testing.h"
 
-namespace {
+namespace io {
 
-TEST(EmptyTest) {
-  // TODO
+class TestContext : public Context {
+ public:
+  TestContext() : Context(MonotonicTime::Now()) {}
+  void RequestRead(FdHandle h) override {}
+  void RequestWrite(FdHandle h) override {}
+  void Run(FdHandle h, std::move_only_function<void()> fn) override {}
+};
+
+TEST(SimpleTest) {
+  TestContext tc;
+  EXPECT_NE(0, tc.loop_start().nanos());
 }
 
-}  // namespace
+}  // namespace io
